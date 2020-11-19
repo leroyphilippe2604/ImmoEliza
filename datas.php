2ba2057ef0abbe93ae82647e2e7f73a88c1f169b
Florence Jacobs <florencejacobs85@gmail.com>
2020-11-19 16:33:10 +0100

    php

--- datas.php
+++ datas.php
@@ -12,18 +12,51 @@
 <body>
 
 <?php
-        if (!empty($_POST['post_code']) && !empty($_POST['rooms_number']) && !empty($_POST['garden']) && !empty($_POST["area"]) && !empty($_POST["living_area"]) && !empty($_POST["bathrooms_number"]) && !empty($_POST["parking"]) && !empty($_POST["pool"]) && !empty($_POST["type"])) {
+        if (isset($_POST["post_code"], $_POST['rooms_number'], $_POST['living_area'], $_POST['pool']) && !empty($_POST['post_code']) && !empty($_POST['rooms_number']) && !empty($_POST["living_area"]) && !empty($_POST["pool"])) {
 
         /* Variable */
         $postcode = $_POST['post_code'];
         $rooms = $_POST['rooms_number'];
-        $garden = $_POST['garden'];
-        $area = $_POST['area'];
         $living = $_POST['living_area'];
-        $bathrooms = $_POST['bathrooms_number'];
-        $parking = $_POST['parking'];
         $pool = $_POST['pool'];
-        $type = $_POST['type'];
+        
+        // $type = $_POST['type']; !! Ajouter !empty dans la première condition
+        // $area = $_POST['area'];
+        // $bathrooms = $_POST['bathrooms_number'];
+        // $parking = $_POST['parking'];
+
+        // 1. Sanitisation
+        filter_var($postcode, FILTER_VALIDATE_INT);
+        filter_var($rooms, FILTER_VALIDATE_INT);
+        filter_var($pool, FILTER_VALIDATE_BOOLEAN);
+        filter_var($living, FILTER_VALIDATE_INT);
+        // filter_var($type, FILTER_VALIDATE_BOOLEAN);
+
+        // 2. Validation
+        if (true ==== filter_var($postcode, FILTER_VALIDATE_INT)) {
+            //execute
+            } else {
+            echo "Please enter a valid post code between 1000 and 9000."
+            };
+
+        if (true ==== filter_var($rooms, FILTER_VALIDATE_INT)) {
+            //execute
+            } else {
+            echo "Please enter un number."
+            };
+
+        if (true ==== filter_var($living, FILTER_VALIDATE_INT)) {
+            //execute
+            } else {
+            echo "Please enter a number in sqm."
+            };
+
+        if (true ==== filter_var($pool, FILTER_VALIDATE_BOOLEAN)) {
+            //execute
+            } else {
+            echo "Please enter if there is a pool or not."
+            };
+        
         }
     
     ?>
