20275aeb466a00a2f0861a64ded1d264100d9c4f
sanchez <frederic.sf@hotmail.com>
2020-11-23 09:28:35 +0100

    je veux pull

--- index.php
+++ index.php
@@ -8,9 +8,16 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <!--CSS File-->
     <link rel="stylesheet" href="./style.css">
-
 </head>
 <body>
+
+<!-- <?php /*
+if (isset($_SESSION['logged'])) {
+echo'<section>';
+    echo'<article>';
+         include ('login.php');
+    echo'</article>';
+echo'</section>'; */ 
     <!--Navbar-->
     <div class="container-fluid p-0">
         <div class="row">
@@ -53,30 +60,14 @@
         <div class="row">
             <div class="col">
                 <!--critere de recherche-->
-                <form class="d-flex flex-column" method="post" action="datas.php">
+                <form class="d-flex flex-column" method="post" action="">
                     <div>
                         <!--Code Postal-->
                         <label for="post_code">Post Code</label>
                         <input type="number" class="post_code" name="post_code">
                     </div>
-                    <div class="d-flex align-items-baseline">
-                        <input type="radio" name="type" value="apartment">
-                        <label for="type">Apartment</label>
-                        <input type="radio" name="type" value="house">
-                        <label for="type">House</label>
-                    </div>
                     <div>
-                        <!--Area-->
-                        <label for="area">Area in ares</label>
-                        <input type="number" class="area" name="area">
-                    </div>
-                    <div>
-                        <!--Living Area-->
-                        <label for="living_area">Living area in ares</label>
-                        <input type="number" class="living_area" name="living_area">
-                    </div>
-                    <div>
-                        <!--Number rooms-->
+                        <!--Number rooms-->lk
                         <label for="rooms_number">Number of rooms</label>
                         <input type="number" min="0" max="12" class="rooms_number" name="rooms_number">
                     </div>
@@ -88,7 +79,16 @@
                         <input type="radio" name="garden" value="no">
                         <label for="garden">No</label>
                     </div>
-
+                    <div>
+                        <!--Area-->
+                        <label for="area">Area in ares</label>
+                        <input type="number" class="area" name="area">
+                    </div>
+                    <div>
+                        <!--Living Area-->
+                        <label for="living_area">Living area in ares</label>
+                        <input type="number" class="living_area" name="living_area">
+                    </div>
                     <div>
                         <!--Number bathrooms-->
                         <label for="bathrooms_number">Number of rooms</label>
@@ -116,6 +116,12 @@
                         <input type="number" min="0" max="6" class="floors_number" name="floors">
                         <!--Type of Property-->
                     </div>
+                    <div class="d-flex align-items-baseline">
+                        <input type="radio" name="type" value="apartment">
+                        <label for="type">Apartment</label>
+                        <input type="radio" name="type" value="house">
+                        <label for="type">House</label>
+                    </div>
                     <div>
                         <input type="submit" value="Get an estimate">
                     </div>
@@ -127,7 +133,6 @@
 
     
     <!--Footer-->
-
 <!--Script Boostrap-->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
