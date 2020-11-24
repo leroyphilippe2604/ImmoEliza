39c91189399bff2bb7f3e5a3d21c8868e16ee60c
Florence Jacobs <florencejacobs85@gmail.com>
2020-11-24 11:34:25 +0100

    bugresolved

--- datas.php
+++ datas.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Dealer</title>
-    <!--Boostrap-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <!--CSS File-->
-    <link rel="stylesheet" href="./style.css">
-</head>
-<body>
-
 <?php
         if (isset($_POST["postal_code"], $_POST['house_m2']) ) {
             echo 'bonjour';
@@ -90,6 +77,3 @@
         //     curl_easy_cleanup(curl);
     
     ?>
-    
-</body>
-</html>
