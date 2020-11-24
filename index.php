c7cc102028670d39068a4af186d84050dcf9ed92
Florence Jacobs <florencejacobs85@gmail.com>
2020-11-24 11:40:34 +0100

    css

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ElizaImmo</title>
+    <!--CSS File-->
+    <link rel="stylesheet" href="./assets/css/style.css">
     <!--Boostrap-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <!--Fontawesome-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
-    <!--CSS File-->
-    <link rel="stylesheet" href="./assets/css/style.css">
 
 </head>
 <body>
